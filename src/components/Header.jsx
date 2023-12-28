53e712fb7d2e21dc1883dfe2511e3a27d3428783
svetk0 <sv.solomatnikova@gmail.com>
2023-12-28 21:22:04 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+//import "./HeroesCards.css";
+
+function Header() {
+ 
+    return (
+        < >
+            <h1 className="header">English study app</h1>
+        </>)
+}
+export default Header;
