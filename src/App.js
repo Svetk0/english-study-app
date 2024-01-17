2175fd83e0fa05113a4fc86a2f3107e4a63663f9
svetk0 <sv.solomatnikova@gmail.com>
2024-01-17 18:57:33 +0300

    toggle buttons added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useEffect }  from 'react';
+import { useEffect, useState }  from 'react';
 import Header from './components/header/Header';
 import Card from './components/card/CardWord';
 import TrainMode from './components/train-mode/TrainMode';
@@ -7,17 +7,27 @@ import TrainMode from './components/train-mode/TrainMode';
 import './App.css';
 
 function App() {
-  useEffect(() => { 
+  useEffect(() => {
     document.title = "English study";
   }, []);
-  
+
+  const [isCardVisible, setIsCardVisible] = useState(false);
+  const [isTrainModeVisible, setIsTrainModeVisible] = useState(false);
+
+  function toggleCardVisibility() {
+    setIsCardVisible(prevIsVisible => !prevIsVisible)
+  }
+
+  function toggleTrainModeVisibility() {
+    setIsTrainModeVisible(prevIsVisible => !prevIsVisible)
+  }
+
   return (
     <div className="App-container">
-      
-      <Header />
-   
-      <Card />
-      <TrainMode />
+      <Header toggleCardVisibility={toggleCardVisibility} toggleTrainModeVisibility={toggleTrainModeVisibility}/>
+
+      {isCardVisible && <Card/>}
+      {isTrainModeVisible && <TrainMode/>}
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,17 @@
 import "./header.css";
-import TrainMode from '../train-mode/TrainMode';
-import Card from '../card/CardWord';
+
 // <h1 className={styles.header}>English study app</h1>
 
-function showMenuPart(Part) {
-    
-    return (
-        <>
-            { console.log('render the btn function')}
-            <Part/>
-        </>
-    )
 
-}
-
-function showCard() { 
-    
-    return (
-        <>
-             { console.log('render Card')}
-        <Card/>
-        </>
-       
-
-    )
-}
-function Header() {
+function Header({toggleCardVisibility, toggleTrainModeVisibility})  {
   return (
     <>
       <div className="header-container">
         <h1 className="header">English study app</h1>
         <div className="menu-header">
                   <button className="button btn-vocabulary" > Vocabulary </button>
-                  <button className="button btn-cards" onClick={showCard}> Cards </button>
-          <button className="button btn-trainMode"> Train mode </button>
+                  <button className="button btn-cards" onClick={toggleCardVisibility}> Cards</button>
+          <button className="button btn-trainMode" onClick={toggleTrainModeVisibility}> Train mode</button>
         </div>
       </div>
     </>
