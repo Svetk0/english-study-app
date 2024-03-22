eb76a8ee290af4c39639e1056d744e288fca182f
svetk0 <sv.solomatnikova@gmail.com>
2024-03-22 19:26:46 +0300

    add comments

--- src/pages/CardPage/CardPage.jsx
+++ src/pages/CardPage/CardPage.jsx
@@ -13,58 +13,57 @@ const CardPage = ({ data }) => {
     let indexInc = index + 1;
     if (indexInc > data.length - 2) {
       setNextBtnActive(false);
-      //setIndex(data.length-1);
     }
     if (indexInc < 1) {
       setBackBtnActive(false);
-      // setIndex(0);
     }
     console.log("currIndex:", indexInc, btnNextActive);
-    //return index;
   };
 
+  //Button NEXT
   const moveNext = () => {
     setBackBtnActive(true);
-   setCliked(false);
-      setIndex(index + 1);
-      if (index > data.length - 3) {
-        console.log("BTN next If index:", index);
-        setNextBtnActive(false);
-  
-        //setIndex(data.length-1);
-      }
-    //checkCardNumbers();
+    setCliked(false);
+    setIndex(index + 1);
+    if (index > data.length - 3) {
+      console.log("BTN next If index:", index);
+      setNextBtnActive(false);
+    }
+
     console.log("BTN next index:", index);
   };
+
+    //Button BACK
   const moveBack = () => {
     console.log("BTN back INPUT index:", index);
     setCliked(false);
     setNextBtnActive(true);
     setIndex(index - 1);
-    //checkCardNumbers();
     if (index < 2) {
-      //console.log("BTN back If index:", index);
       setBackBtnActive(false);
       setIndex(0);
     }
-
-    //console.log('BTN back OUTPUT index:',index);
   };
+
+  //Button CHECK
   const [clicked, setCliked] = useState(false);
   const handleCheck = () => {
     setCliked(!clicked);
   };
+
+  
   return (
     <>
       <h2 className="title">CardWord</h2>
       <div className={styles.cardlist_container}>
-              <button
-                  className={ styles.button}
-                  disabled={!btnBackActive}
-                  onClick={moveBack}>
-        Back
-      </button>
-              <div>
+        <button
+          className={styles.button}
+          disabled={!btnBackActive}
+          onClick={moveBack}
+        >
+          Back
+        </button>
+        <div>
           {console.log("curr render index:", index)}
 
           <Card
@@ -73,18 +72,18 @@ const CardPage = ({ data }) => {
             word={data[index].word}
             clicked={clicked}
             btnFunction={handleCheck}
-          
           />
           <p>
             {index + 1}/{data.length}
           </p>
-              </div>
-              <button
-                  className={ styles.button}
-                  disabled={!btnNextActive}
-                  onClick={moveNext}>
-        Next
-      </button>
+        </div>
+        <button
+          className={styles.button}
+          disabled={!btnNextActive}
+          onClick={moveNext}
+        >
+          Next
+        </button>
       </div>
     </>
   );
