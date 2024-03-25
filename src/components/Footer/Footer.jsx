df843ae64607165da8186b4737508d1037f57726
svetk0 <sv.solomatnikova@gmail.com>
2024-03-26 00:01:57 +0300

    fix bugs in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,10 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
       
-        <div className={ styles.footer_wrapper }> 
+        <div className={styles.footer_wrapper}> 
+            <a className={styles.a_container} href="https://github.com/Svetk0/english-study-app" target="_blank"> 
         <div className={ styles.text }>developed by </div> 
-            <a href="https://github.com/Svetk0/english-study-app" target="_blank">
+            
             
                 <img className={styles.img} src={img} alt="github" />
             </a>
