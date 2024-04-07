bd2e2b85793e0e8051cd31decc0c7a398425e5f9
svetk0 <sv.solomatnikova@gmail.com>
2024-04-07 18:58:51 +0300

    add styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,30 +5,27 @@ const Form = () => {
   const [text, setText] = useState("");
   const [editedText, setEditedText] = useState("");
   const handleSubmit = (e) => {
-      e.preventDefault();
-      setEditedText(modifyText(text));
-      setText('');
-    
+    e.preventDefault();
+    setEditedText(modifyText(text));
+    setText("");
   };
-    
-    const handleChange = (e) => {
-        setEditedText('');
-        setText(e.target.value)
-    };
-    const modifyText = (content) => { 
-        const styledText =content.toUpperCase();
-        return styledText;
 
-    }
-   
+  const handleChange = (e) => {
+    setEditedText("");
+    setText(e.target.value);
+  };
+  const modifyText = (content) => {
+    const styledText = content.toUpperCase();
+    return styledText;
+  };
 
   return (
-      <div className={ styles.form__wrapper}>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange}  value={text} />
-        <input type="submit" value="Submit" />
-          </form>
-          <p >Your text: {editedText}</p>
+    <div className={styles.form__wrapper}>
+      <form onSubmit={handleSubmit}  className={styles.form}>
+        <input className={styles.form__input} onChange={handleChange} value={text} />
+        <input className={styles.form__button} type="submit" value="Submit" />
+      </form>
+      <p>Your text: {editedText}</p>
     </div>
   );
 };
