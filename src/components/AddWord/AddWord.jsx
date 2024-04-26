3be8e20c7dd1d697fb9ea7503c45c7cb6062b351
svetk0 <sv.solomatnikova@gmail.com>
2024-04-26 19:49:41 +0300

    add errors styles for empty lines

--- src/components/AddWord/AddWord.jsx
+++ src/components/AddWord/AddWord.jsx
@@ -4,54 +4,63 @@ import { API_ADD_WORD } from "../../utils/apiUrls";
 //import { WordContext } from "../../context/WordContext/WordContext";
 
 const AddWord = () => {
-    const [isSubmit, setIsSubmit] = useState(false);
-    const [isSubmitDisabled, setIsSubmitDisabled] = useState('');
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState("");
   const [text, setText] = useState({
     id: "",
     english: "",
     transcription: "",
     russian: "",
   });
+  const [errors, setErrors] = useState({
+    english: false,
+    transcription: false,
+    russian: false,
+  });
   const [editedText, setEditedText] = useState("");
   //const { words, setWords } = useContext(WordContext);
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setEditedText({ ...text });
-        postNewWord();
-        setIsSubmit(true);
-        setText({
-            id: "",
-            english: "",
-            transcription: "",
-            russian: "",
-        });
-        setIsSubmitDisabled('disabled');
-    };
-    const checkEmptyLines = () => { 
-        if ((text.english.trim() === '') ||
-            (text.transcription.trim() === '') ||
-            (text.russian.trim() === ''))
-        {
-            setIsSubmitDisabled('disabled');
-        }    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEditedText({ ...text });
+    postNewWord();
+    setIsSubmit(true);
+    setText({
+      id: "",
+      english: "",
+      transcription: "",
+      russian: "",
+    });
+    setIsSubmitDisabled("disabled");
+  };
+  const checkEmptyLines = () => {
+    if (
+      text.english.trim() === "" ||
+      text.transcription.trim() === "" ||
+      text.russian.trim() === ""
+    ) {
+      setIsSubmitDisabled("disabled");
     }
-    useEffect(() => { checkEmptyLines(); }, [text]);
+  };
+  useEffect(() => {
+    checkEmptyLines();
+  }, [text]);
 
   const handleChange = (event) => {
-      setEditedText("");
-      setIsSubmitDisabled('');
-      setIsSubmit(false);
+    setEditedText("");
+    setIsSubmitDisabled("");
+    setIsSubmit(false);
     setText((prevValue) => {
       return { ...prevValue, [event.target.name]: event.target.value };
     });
+    setErrors({ ...errors, [event.target.name]: !event.target.value.trim() });
   };
 
   const postNewWord = async () => {
     try {
-        text.id = Math.random() * 10;
-        //setWords([...words, text]);
-        console.log("newPost", text);
-       
+      text.id = Math.random() * 10;
+      //setWords([...words, text]);
+      console.log("newPost", text);
+
       const response = await fetch(API_ADD_WORD, {
         method: "POST",
         //add object
@@ -74,8 +83,9 @@ const AddWord = () => {
             {" "}
             word
           </label>
-          <input
-            className={styles.form__input}
+                  <input
+                      className = { errors.english  ? styles.errorInput : styles.form__input}
+       
             onChange={handleChange}
             value={text.english}
             name="english"
@@ -87,32 +97,40 @@ const AddWord = () => {
             transcription
           </label>
           <input
-            className={styles.form__input}
+            className = { errors.transcription  ? styles.errorInput : styles.form__input}
             onChange={handleChange}
             value={text.transcription}
             name="transcription"
           />
         </div>
 
-        <div className={styles.input_wrapper}> 
+        <div className={styles.input_wrapper}>
           <label htmlFor="" name="russian">
             {" "}
             translation
           </label>
           <input
-            className={styles.form__input}
+            className = { errors.russian  ? styles.errorInput : styles.form__input}
             onChange={handleChange}
             value={text.russian}
             name="russian"
           />
-              </div>
-              <div className={styles.input_wrapper}>
-                  <label className={ styles.label_btn} htmlFor=""> ''</label>
-                  <input className={styles.form__button} type="submit" value="Add New" disabled={ isSubmitDisabled} />
-                  </div>
+        </div>
+        <div className={styles.input_wrapper}>
+          <label className={styles.label_btn} htmlFor="">
+            {" "}
+            ''
+          </label>
+          <input
+            className={styles.form__button}
+            type="submit"
+            value="Add New"
+            disabled={isSubmitDisabled}
+          />
+        </div>
       </form>
       <div className={isSubmit ? styles.newWord : styles.newWord__off}>
-       <b>You've added a new word:</b> 
+        <b>You've added a new word:</b>
         <br /> english: {editedText.english}
         <br /> transcription: {editedText.transcription}
         <br /> translation: {editedText.russian}
