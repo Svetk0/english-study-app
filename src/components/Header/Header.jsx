8c393cdbb4493c404221f203f1298dcbbe82b0e2
svetk0 <sv.solomatnikova@gmail.com>
2024-04-29 19:30:28 +0300

    fix HEADER bugs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,27 +3,29 @@ import { Link } from "react-router-dom";
 
 
 
-import "./Header.css";
+import styles from "./Header.module.css";
 
 // <h1 className={styles.header}>English study app</h1>
 
 function Header() {
   return (
-    <div className="header-container">
-      <h1 className="header"><Link to="/english-study-app">English study app</Link></h1>
-      <nav className="menu-header">
-        <ul className="menu-list__container">
-          <li className="menu-link">
+    <div className={ styles.header_container} >
+      <h1 className={ styles.header}><Link to="/english-study-app">English study app</Link></h1>
+      <nav className={ styles.menuHeader}>
+        <ul className={ styles.menuList__container}>
+          <li className={ styles.menuLink}>
             <Link to="/english-study-app/vocabulary">Vocabulary</Link>
           </li>
-         
-          <li className="menu-link">
+          {/* <li className={ styles.menuLink}>
+            <Link to="/english-study-app/vocabulary-list">V List</Link>
+          </li> */}
+          <li className={ styles.menuLink}>
             <Link to="/english-study-app/cards">Cards</Link>
           </li>
-          {/* <li className="menu-link">
+          <li className={ styles.menuLink}>
             <Link to="/english-study-app/train">Train mode</Link>
-          </li> */}
-          <li className="menu-link">
+          </li>
+          <li className={ styles.menuLink}>
             <Link to="/english-study-app/form">Search</Link>
           </li>
         </ul>
